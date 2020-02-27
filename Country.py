a3b76dbf55e12bedbcd5d67ae271ef6b509108b3
Jake Boersma <jacob.m.boersma@vanderbilt.edu>
2020-02-27 02:08:51 -0600

    create initial state

---
+++ Country.py
@@ -0,0 +1,25 @@
+"""
+Project Part 1b
+
+Team 6
+
+Colin Moody, Ohad Beck, Charlie MacVicar, Jake Boersma
+
+"""
+
+import sys
+
+
+class Country():
+
+    def __init__(self, name, resource_dict, weight_dict):
+        self.name = name                    # country name
+        self.resources = resource_dict      # dictionary containing amount of resources the country possesses
+        self.weights = weight_dict          # dictionary containing resources and corresponding weights
+
+def main(argv):
+    print("")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
